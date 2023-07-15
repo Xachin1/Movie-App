1eff453a3039ddf63f8f1dbece2bf82261fecbb3
Xachin1 <sbhandary123@gmail.com>
2023-07-15 22:48:53 +1000

    final push

--- app.js
+++ app.js
@@ -1,46 +1,42 @@
-// http://www.omdbapi.com/?i=tt3896198&apikey=bdc05ce
+// // http://www.omdbapi.com/?i=tt3896198&apikey=bdc05ce
 
-const movieResultsEl = document.querySelector(' .movie__results');
-const searchTitleEl = document.querySelector(' .title');
-const loadingSpinEl = document.querySelector(' .loading');
-const sortEL = document.querySelector('#sort');
+const movieResultsEl = document.querySelector('.movie__results');
+const searchTitleEl = document.querySelector('.title');
+const loadingSpinEl = document.querySelector('.loading');
+const sortEl = document.querySelector('#sort');
 
 async function renderMovies(sort) {
   setTimeout(() => {
     loadingSpinEl.classList += ' movies__loading';
-  }, 400);
+  }, 500);
 
   movieResultsEl.style.visibility = 'hidden';
+
   const movieTitles = await fetch(
-    `http://www.omdbapi.com/?i=tt3896198&apikey=bdc05ce&s=${word}`
+    `http://www.omdbapi.com/?i=tt3896198&apikey=bdc05ce&s=${title}`
   );
   const movieTitlesData = await movieTitles.json();
-  console.log(movieTitlesData);
 
-  if (movieTitlesData.Search) {
-    const movieTitlesArray = movieTitlesData.Search.slice(0, 8);
-    setTimeout(() => {
-      movieResultsEl.style.visibility = 'visible';
-      movieResultsEl.innerHTML = movieTitlesArray
-        .map((movie) => movieHTML(movie))
-        .join('');
-      searchTitleEl.innerHTML = `Results for: ${word}`;
-      sortEL.style.display = 'flex';
-      loadingSpinEl.classList.remove('movies__loading');
-    }, 1200);
+  const movieTitlesArray = movieTitlesData.Search.slice(0, 8);
 
-    if (sort === 'NEW_TO_OLD') {
-      movieTitlesArray.sort((a, b) => b.Year - a.Year);
-    } else if (sort === 'OLD_TO_NEW') {
-      movieTitlesArray.sort((a, b) => a.Year - b.Year);
-    }
-  } else {
-    // Handle case where no search results are found
-    movieResultsEl.innerHTML = '<p>No results found</p>';
+  if (sort === 'NEW_TO_OLD') {
+    movieTitlesArray.sort((a, b) => b.Year - a.Year);
+  } else if (sort === 'OLD_TO_NEW') {
+    movieTitlesArray.sort((a, b) => a.Year - b.Year);
   }
+
+  setTimeout(() => {
+    movieResultsEl.style.visibility = 'visible';
+    movieResultsEl.innerHTML = movieTitlesArray
+      .map((movie) => movieHTML(movie))
+      .join('');
+    searchTitleEl.innerHTML = `Results for: ${title}`;
+    sortEl.style.display = 'flex';
+    loadingSpinEl.classList.remove('movies__loading');
+  }, 1200);
 }
 function onSearchChange(event) {
-  word = event.target.value;
+  title = event.target.value;
   event.preventDefault();
   renderMovies();
 }
@@ -57,12 +53,4 @@ function movieHTML(movie) {
 
 function sortMovieYear(event) {
   renderMovies(event.target.value);
-}
-
-function openMenu() {
-  document.body.classList += ' menu--open';
-}
-
-function closeMenu() {
-  document.body.classList.remove('menu--open');
 }
